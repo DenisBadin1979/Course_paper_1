c6aec2547a76f756ad1e19bba841fcd751354ba4
Denis_Badin <denisbadin@yandex.ru>
2025-07-13 22:59:57 +0500

    не законченный вариант

--- src/main.py
+++ src/main.py
@@ -1,5 +1,20 @@
+from json.decoder import NaN
+
+from numpy import nan
+
 from utils import greeting_user, reader_transaction_excel
 
 
 
-print(reader_transaction_excel('data/operations.xlsx'))
+dict_from_excel = (reader_transaction_excel('data/operations.xlsx'))
+list_number_kart = []
+for ikart in dict_from_excel:
+    unic_kart  = ikart.get("Номер карты")
+    if unic_kart not in list_number_kart and type(unic_kart) != float:
+        list_number_kart.append(unic_kart)
+    for i_num_kart in list_number_kart:
+        total_kart = 0
+        if unic_kart == i_num_kart:
+            total_kart == int (ikart.get("Сумма платежа"))
+            total_kart +=total_kart
+        print(total_kart)
