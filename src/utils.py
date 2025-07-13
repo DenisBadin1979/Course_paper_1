8a68a966eb675d19e50ba0296f149903a918e9c0
Denis_Badin <denisbadin@yandex.ru>
2025-07-13 13:43:58 +0500

    функция приветсвие

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,14 @@
 import datetime
 
-def greeting_user () -> str:
+
+def greeting_user() -> str:
+    """Приветствие в формате Доброе утро/Добрый день/Добрый вечер/Доброй ночи в зависимости от текущего времени"""
     dict_time = {
-"Доброе утро":[6, 7, 8, 9, 10],
-"Добрый день":[11, 12, 13, 14, 15],
-"Добрый вечер":[16, 17, 18, 19, 20, 21, 22],
-"Доброй ночи":[0, 1, 2, 3, 4, 5, 23],
-             }
+        "Доброе утро": [6, 7, 8, 9, 10],
+        "Добрый день": [11, 12, 13, 14, 15],
+        "Добрый вечер": [16, 17, 18, 19, 20, 21, 22],
+        "Доброй ночи": [0, 1, 2, 3, 4, 5, 23],
+    }
 
     current_date_time = datetime.datetime.now()
     current_time = current_date_time.time()
@@ -14,6 +16,5 @@ def greeting_user () -> str:
     for list_hours in dict_time:
         period_hours = dict_time[list_hours]
         if hour in period_hours:
-           return (list_hours)
-
-
+            greetung = list_hours
+    return greetung
