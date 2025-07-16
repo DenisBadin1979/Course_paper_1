418e4ecb63b93ae4485bf8adc847f3c8b257e507
Denis_Badin <denisbadin@yandex.ru>
2025-07-16 21:24:40 +0500

    Для главной страницы 1,2,3

--- src/main.py
+++ src/main.py
@@ -1,29 +1,5 @@
-from json.decoder import NaN
-import pandas as pd
 
-from numpy import nan
 
-from utils import greeting_user, reader_transaction_excel
 
-name_path = 'data/operations.xlsx'
-df = reader_transaction_excel(name_path)
-df = df.drop(['Дата операции', 'Дата платежа', 'Статус','Сумма операции','Валюта операции','Валюта платежа',
-              'Кэшбэк', 'Категория', 'MCC', 'Описание', 'Бонусы (включая кэшбэк)', 'Округление на инвесткопилку',
-              'Сумма операции с округлением'], axis=1)
-df = df.dropna()
-df = df.groupby('Номер карты')['Сумма платежа'].sum()
-df_dict = df.to_dict()
-print(df)
 
-# dict_from_excel = (reader_transaction_excel('data/operations.xlsx'))
-# list_number_kart = []
-# for ikart in dict_from_excel:
-#     unic_kart  = ikart.get("Номер карты")
-#     if unic_kart not in list_number_kart and type(unic_kart) != float:
-#         list_number_kart.append(unic_kart)
-#     for i_num_kart in list_number_kart:
-#         total_kart = 0
-#         if unic_kart == i_num_kart:
-#             total_kart == int (ikart.get("Сумма платежа"))
-#             total_kart +=total_kart
-#         print(total_kart)
+
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 import datetime
+
+import pandas
 import pandas as pd
 
 
@@ -8,7 +10,7 @@ def greeting_user() -> str:
         "Доброе утро": [6, 7, 8, 9, 10],
         "Добрый день": [11, 12, 13, 14, 15],
         "Добрый вечер": [16, 17, 18, 19, 20, 21, 22],
-        "Доброй ночи": [0, 1, 2, 3, 4, 5, 23],
+        "Доброй ночи": [0, 1, 2, 3, 4, 5, 23]
     }
 
     current_date_time = datetime.datetime.now()
@@ -17,18 +19,66 @@ def greeting_user() -> str:
     for list_hours in dict_time:
         period_hours = dict_time[list_hours]
         if hour in period_hours:
-            greetung = list_hours
-    return greetung
+            hello_user = list_hours
+    return hello_user
 
 def reader_transaction_excel(file_path: str) -> list[dict]:
-    """Принимает на данные с транзакциями в формате excel  и возвращает список словарей"""
+    """Принимает на данные с транзакциями в формате excel и возвращает dataframe и фильтрует его по операциям
+    за период с начала месяца, на который выпадает входящая дата, по входящую дату"""
     try:
         df = pd.read_excel(file_path)
+        date_end = datetime.datetime.now()
+        data_start = date_end.replace(day=1)
+        df['Дата платежа'] = pd.to_datetime(df['Дата платежа'], format='%d.%m.%Y')
+        df = df[(df['Дата платежа'] >= data_start) & (df['Дата платежа'] <= date_end)]
         return df
     except FileNotFoundError:
         raise FileNotFoundError(f"По заданному пути {file_path} ничего не найдено")
     except Exception as e:
         raise Exception(f"Ошибка {e}")
 
+def total_card (df: pandas) -> list[dict]:
+    """Принимает dataframe из функции reader_transaction_excel и возвращает траты по каждой карте в списке словарей"""
+    try:
+        df = df.drop(['Дата операции', 'Дата платежа', 'Статус', 'Сумма операции', 'Валюта операции', 'Валюта платежа',
+                      'Кэшбэк', 'Категория', 'MCC', 'Описание', 'Бонусы (включая кэшбэк)',
+                      'Округление на инвесткопилку', 'Сумма платежа'], axis=1)
+        df = df.dropna()
+        df['Номер карты'] = df['Номер карты'].str.replace('*', '', regex=False)
+        df = df.groupby('Номер карты')['Сумма операции с округлением'].sum()
+        df_dict = df.to_dict()
+        cards_list = []
+        for key, value in df_dict.items():
+            card_dict = {"last_digital": key, "total_spent": round(value, 2),
+                         "cashback": round((value * 0.01), 2)}
+            cards_list.append(card_dict)
+        return cards_list
+    except Exception as e:
+        raise Exception(f"Ошибка {e} столбец не найден")
+
+def total_transaction(df: pandas) -> list[dict]:
+    try:
+        df = df.drop(['Дата операции', 'Номер карты', 'Статус', 'Сумма операции', 'Валюта операции', 'Валюта платежа',
+                      'Кэшбэк', 'MCC', 'Бонусы (включая кэшбэк)',
+                      'Округление на инвесткопилку', 'Сумма платежа'], axis=1)
+        df = df.nlargest(5, 'Сумма операции с округлением')
+        df = df.reset_index(drop=True)
+        df['Дата платежа'] = pd.to_datetime(df['Дата платежа']).dt.strftime('%d.%m.%Y')
+        df_list = df.to_dict(orient='records')
+        transaction_list = []
+        for key in df_list:
+            transaction_list.append({'date': key['Дата платежа'], 'amount': key['Сумма операции с округлением'],
+                                     'category': key['Категория'], 'description': key['Описание']})
+        return transaction_list
+    except Exception as e:
+        raise Exception(f"Ошибка {e} столбец не найден")
+
+
+
 name_path = 'data/operations.xlsx'
-print(reader_transaction_excel(name_path))
+dt_period = reader_transaction_excel(name_path)
+trat = total_card(dt_period)
+tran = total_transaction(dt_period)
+print(greeting_user())
+print(trat)
+print(tran)
