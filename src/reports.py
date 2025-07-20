df725f34287addda889bfed5cbc3918b1b469eb3
Denis_Badin <denisbadin@yandex.ru>
2025-07-20 20:17:21 +0500

    Все до основноых тестов (flake8, black, isort, mypy)  и нет readme

--- src/reports.py
+++ src/reports.py
@@ -1,7 +1,28 @@
+import logging
 from datetime import datetime
+from operator import index
 from typing import Optional
-
+import datetime
 import pandas as pd
+from openpyxl.utils.datetime import to_excel
+
+
+def my_decorator_noarg(func):
+    def wrapper(*args, **kwargs):
+        df = func(*args, **kwargs)
+        df.to_excel('data/report.xlsx',index=False, sheet_name='report')
+    return wrapper
+
+def my_decorator_arg (file_record: str):
+    def my_decorator (func):
+        def inner (*args, **kwargs):
+            result = func(*args, **kwargs)
+            result.to_excel(file_record, index=False, sheet_name='report')
+        return inner
+    return my_decorator
+
+
+
 
 
 def spending_by_category(transactions: pd.DataFrame,
@@ -19,14 +40,17 @@ def spending_by_category(transactions: pd.DataFrame,
         date_start = date_end.replace(year=num_y, month=num_m, day=1)
     else:
         date_start = date_end.replace(month=num_m, day=1)
-
     df = transactions
     df['Дата платежа'] = pd.to_datetime(df['Дата платежа'], format='%d.%m.%Y')  # форматируем в исходном файле дату
-    df = df[(df['Дата платежа'] >= date_start) & (df['Дата платежа'] <= date_end)]  # оставляем операции в периоде
-    df = df.drop(['Дата операции', 'Номер карты', 'Статус', 'Сумма операции', 'Валюта операции', 'Валюта платежа',
-                  'Кэшбэк', 'MCC', 'Бонусы (включая кэшбэк)',
-                  'Округление на инвесткопилку', 'Сумма платежа', 'Дата платежа', 'Описание'], axis=1)
+    df = df[
+        (df['Дата платежа'] >= date_start) &
+        (df['Дата платежа'] <= date_end) &
+        (df['Категория'].isin([category]))
+        ]
+    df = df.groupby('Категория')['Сумма операции с округлением'].sum()
     return df
 
-trans =  pd.read_excel('data/operations.xlsx')
-print(spending_by_category(trans, "Госуслуги", "25.06.2025"))
+tran = pd.read_excel('data/operations.xlsx')
+df_s = spending_by_category(tran, "Транспорт", "25.06.2025")
+# print(df_s)
+# df_s.to_excel('data/report.xlsx',index=False, sheet_name='report')
