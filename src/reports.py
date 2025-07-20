a89e1a9d0470bf2aa978c838d0faeed1ade55416
Denis_Badin <denisbadin@yandex.ru>
2025-07-20 21:05:53 +0500

    mypy не прошел

--- src/reports.py
+++ src/reports.py
@@ -1,56 +1,49 @@
-import logging
-from datetime import datetime
-from operator import index
-from typing import Optional
 import datetime
+from typing import Optional
+
 import pandas as pd
-from openpyxl.utils.datetime import to_excel
 
 
 def my_decorator_noarg(func):
     def wrapper(*args, **kwargs):
         df = func(*args, **kwargs)
-        df.to_excel('data/report.xlsx',index=False, sheet_name='report')
+        df.to_excel("data/report.xlsx", index=False, sheet_name="report")
+
     return wrapper
 
-def my_decorator_arg (file_record: str):
-    def my_decorator (func):
-        def inner (*args, **kwargs):
+
+def my_decorator_arg(file_record: str):
+    def my_decorator(func):
+        def inner(*args, **kwargs):
             result = func(*args, **kwargs)
-            result.to_excel(file_record, index=False, sheet_name='report')
+            result.to_excel(file_record, index=False, sheet_name="report")
+
         return inner
+
     return my_decorator
 
 
-
-
-
-def spending_by_category(transactions: pd.DataFrame,
-                         category: str,
-                         date: Optional[str] = None) -> pd.DataFrame:
-    if date == None:
+def spending_by_category(transactions: pd.DataFrame, category: str, date: Optional[str] = None) -> pd.DataFrame:
+    if date is None:
         date_end = datetime.datetime.now()
     else:
         date_end = datetime.datetime.strptime(date, "%d.%m.%Y")
 
-    num_y = int(date_end.strftime('%Y')) - 1
-    num_m = int(date_end.strftime('%m')) - 2
-    if num_m<=0:
+    num_y = int(date_end.strftime("%Y")) - 1
+    num_m = int(date_end.strftime("%m")) - 2
+    if num_m <= 0:
         num_m = 12 + num_m
         date_start = date_end.replace(year=num_y, month=num_m, day=1)
     else:
         date_start = date_end.replace(month=num_m, day=1)
     df = transactions
-    df['Дата платежа'] = pd.to_datetime(df['Дата платежа'], format='%d.%m.%Y')  # форматируем в исходном файле дату
-    df = df[
-        (df['Дата платежа'] >= date_start) &
-        (df['Дата платежа'] <= date_end) &
-        (df['Категория'].isin([category]))
-        ]
-    df = df.groupby('Категория')['Сумма операции с округлением'].sum()
+    df["Дата платежа"] = pd.to_datetime(df["Дата платежа"], format="%d.%m.%Y")  # форматируем в исходном файле дату
+    df = df[(df["Дата платежа"] >= date_start) & (df["Дата платежа"] <= date_end) & (df["Категория"].isin([category]))]
+    df = df.groupby("Категория")["Сумма операции с округлением"].sum()
     return df
 
-tran = pd.read_excel('data/operations.xlsx')
+
+tran = pd.read_excel("data/operations.xlsx")
 df_s = spending_by_category(tran, "Транспорт", "25.06.2025")
 # print(df_s)
 # df_s.to_excel('data/report.xlsx',index=False, sheet_name='report')
--- src/views.py
+++ src/views.py
@@ -1,37 +1,35 @@
 import json
 from json import JSONEncoder
-from locale import currency
 
-from src.utils import greeting_user, reader_transaction_excel, total_card, total_transaction, currency_converter, \
-    stock_sandp500
+from src.utils import (
+    currency_converter,
+    greeting_user,
+    reader_transaction_excel,
+    stock_sandp500,
+    total_card,
+    total_transaction,
+)
 
 
-def main_page (path_file_data: str) -> JSONEncoder:
+def main_page(path_file_data: str) -> JSONEncoder:
     """Функция предоставляет JSON-ответ с данными о расходах за месяц, сумме кешбэка"""
-    #Вначале приветствие пользователя в соответствии временем суток
+    # Вначале приветствие пользователя в соответствии временем суток
     greetings_time = greeting_user()
     # Формируем дата фрейм с сортировкой по дате месяца
-    df_user = reader_transaction_excel (path_file_data)
+    df_user = reader_transaction_excel(path_file_data)
     # траты по каждой карте
     trat = total_card(df_user)
     # список словарей где указаны  ТОР5 транзакций
-    trans = total_transaction (df_user)
-    user_currencies = currency_converter (["USD", "EUR"])
-    user_stocks = stock_sandp500 (["AAPL", "AMZN", "GOOGL", "MSFT", "TSLA"])
+    trans = total_transaction(df_user)
+    user_currencies = currency_converter(["USD", "EUR"])
+    user_stocks = stock_sandp500(["AAPL", "AMZN", "GOOGL", "MSFT", "TSLA"])
 
     dict_end = {
-                "greeting" : greetings_time,
-                "cards" : trat,
-                "top_transactions": trans,
-                "currency_rates": user_currencies,
-                "stock_prices": user_stocks
-                }
+        "greeting": greetings_time,
+        "cards": trat,
+        "top_transactions": trans,
+        "currency_rates": user_currencies,
+        "stock_prices": user_stocks,
+    }
     json_data = json.dumps(dict_end)
     return json_data
-
-
-
-
-
-
-
